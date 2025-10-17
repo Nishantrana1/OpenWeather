ae8dd35e304e20fd8591b73df27d24f3ec3f8661
Nishant . <94187nishant@gmail.com>
2025-10-17 15:56:34 +0530

    Web+Desktop

--- app.py
+++ app.py
@@ -1,48 +1,83 @@
+# from flask import Flask, render_template, request
+# import requests
+
+# app = Flask(__name__)
+
+# API_KEY = "cb42b6b312dd441cb70170954250910" 
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     weather_data = None
+#     weather_bg = "/static/default.jpg" 
+
+#     if request.method == 'POST':
+#         city = request.form['city']
+#         url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no"
+#         response = requests.get(url)
+#         data = response.json()
+
+        
+#         if "error" not in data:
+#             weather_data = {
+#                 "city": data["location"]["name"],
+#                 "region": data["location"]["region"],
+#                 "country": data["location"]["country"],
+#                 "temperature": data["current"]["temp_c"],
+#                 "condition": data["current"]["condition"]["text"],
+#                 # "icon": "https:" + data["current"]["condition"]["icon"]
+#             }
+
+#             condition = weather_data["condition"].lower()
+#             if "sun" in condition or "clear" in condition:
+#                 weather_bg = "/static/sunny.jpg"
+#                 weather_icon ="/static/sun.png"
+#             elif "rain" in condition or "drizzle" in condition or "shower" in condition:
+#                 weather_bg = "/static/rainy.jpg"
+#                 weather_icon ="/static/drizzle.png"
+#             elif "cloud" in condition or "overcast" in condition:
+#                 weather_bg = "/static/cloudy.jpg"
+#                 weather_icon ="/static/clouds.png"
+#             elif "snow" in condition:
+#                 weather_bg = "/static/snow.jpg"
+#                 weather_icon ="/static/snowflake.png"
+#             elif "storm" in condition or "thunder" in condition:
+#                 weather_bg = "/static/storm.jpg"
+#                 weather_icon ="/static/storm.png"
+#         else:
+#             weather_data = {"error": data["error"]["message"]}
+
+#     return render_template('index.html', weather=weather_data, weather_bg=weather_bg, weather_icon=weather_icon)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# app.py
 from flask import Flask, render_template, request
-import requests
+from weather_utils import get_weather
 
 app = Flask(__name__)
-
-API_KEY = "cb42b6b312dd441cb70170954250910" 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     weather_data = None
-    weather_bg = "/static/default.jpg" 
+    weather_bg = "static/default.jpg"
+    weather_icon = "static/sun.png"
 
     if request.method == 'POST':
         city = request.form['city']
-        url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no"
-        response = requests.get(url)
-        data = response.json()
+        weather_data = get_weather(city)
+        if "error" not in weather_data:
+            weather_bg = weather_data["weather_bg"]
+            weather_icon = weather_data["weather_icon"]
 
-        
-        if "error" not in data:
-            weather_data = {
-                "city": data["location"]["name"],
-                "region": data["location"]["region"],
-                "country": data["location"]["country"],
-                "temperature": data["current"]["temp_c"],
-                "condition": data["current"]["condition"]["text"],
-                "icon": "https:" + data["current"]["condition"]["icon"]
-            }
-
-            # 🌤 Choose background based on weather condition
-            condition = weather_data["condition"].lower()
-            if "sun" in condition or "clear" in condition:
-                weather_bg = "/static/sunny.jpg"
-            elif "rain" in condition or "drizzle" in condition or "shower" in condition:
-                weather_bg = "/static/rainy.jpg"
-            elif "cloud" in condition or "overcast" in condition:
-                weather_bg = "/static/cloudy.jpg"
-            elif "snow" in condition:
-                weather_bg = "/static/snow.jpg"
-            elif "storm" in condition or "thunder" in condition:
-                weather_bg = "/static/storm.jpg"
-        else:
-            weather_data = {"error": data["error"]["message"]}
-
-    return render_template('index.html', weather=weather_data, weather_bg=weather_bg)
+    return render_template(
+        'index.html',
+        weather=weather_data,
+        weather_bg=weather_bg,
+        weather_icon=weather_icon
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
---
+++ weather_utils.py
@@ -0,0 +1,52 @@
+# weather_utils.py
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY") or "cb42b6b312dd441cb70170954250910"
+
+def get_weather(city: str):
+    """Fetch weather data for a given city using WeatherAPI."""
+    url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no"
+    response = requests.get(url)
+    data = response.json()
+
+    if "error" in data:
+        return {"error": data["error"]["message"]}
+
+    condition = data["current"]["condition"]["text"].lower()
+
+    # Choose background and icon
+    if "sun" in condition or "clear" in condition:
+        bg = "static/sunny.jpg"
+        icon = "static/sun.png"
+    elif "rain" in condition or "drizzle" in condition or "shower" in condition:
+        bg = "static/rainy.jpg"
+        icon = "static/drizzle.png"
+    elif "cloud" in condition or "overcast" in condition:
+        bg = "static/cloudy.jpg"
+        icon = "static/clouds.png"
+    elif "snow" in condition:
+        bg = "static/snow.jpg"
+        icon = "static/snowflake.png"
+    elif "storm" in condition or "thunder" in condition:
+        bg = "static/storm.jpg"
+        icon = "static/storm.png"
+    elif "foggy" in condition or "mist" in condition:
+        bg = "static/mist.jpg"
+        icon = "static/mist.png"
+    else:
+        bg = "static/default.jpg"
+        icon = "static/sun.png"
+
+    return {
+        "city": data["location"]["name"],
+        "region": data["location"]["region"],
+        "country": data["location"]["country"],
+        "temperature": data["current"]["temp_c"],
+        "condition": data["current"]["condition"]["text"],
+        "weather_bg": bg,
+        "weather_icon": icon
+    }
