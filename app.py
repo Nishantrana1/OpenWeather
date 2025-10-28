853d0a9244b64794e668454863d1aad1884a3ca4
Nishant . <94187nishant@gmail.com>
2025-10-28 23:37:27 +0530

    made some changes use gmeni in it

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ def index():
     weather_data = None
     weather_bg = "static/default.jpg"
     weather_icon = "static/sun.png"
+    weather_video = "static/default.mp4"
 
     if request.method == 'POST':
         city = request.form['city']
         weather_data = get_weather(city)
         if "error" not in weather_data:
             weather_bg = weather_data["weather_bg"]
+            weather_video = weather_data["weather_video"]
             weather_icon = weather_data["weather_icon"]
 
     return render_template(
         'index.html',
         weather=weather_data,
         weather_bg=weather_bg,
+        weather_video=weather_video,
         weather_icon=weather_icon
     )
 
--- gui_app.py
+++ gui_app.py
@@ -42,7 +42,7 @@ def update_icon(icon_path):
     canvas.itemconfig(icon_item, image=weather_icon_tk, state="normal")
 
 def update_weather_info(weather):
-    text = f"{weather['city']}, {weather['country']}\nTemp: {weather['temperature']}°C\nCondition: {weather['condition']}\nHumidity: {weather['humidity']}"
+    text = f"{weather['city']}, {weather['country']}\nTemp: {weather['temperature']}°C\nCondition: {weather['condition']}\nHumidity: {weather['humidity']}\nWind Speed: {weather['windkph']}{weather['winddir']}"
     canvas.itemconfig(info_item, text=text)
 
 root = tk.Tk()
